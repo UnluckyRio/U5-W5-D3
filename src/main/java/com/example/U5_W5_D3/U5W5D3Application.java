6c3341a6b1c4e9c57ab3e7d2eeb06170e4ad4a1a
Valerio Di Felice <valesmile.99@gmail.com>
2025-10-08 09:17:10 +0200

    Initial commit

---
+++ src/main/java/com/example/U5_W5_D3/U5W5D3Application.java
@@ -0,0 +1,13 @@
+package com.example.U5_W5_D3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W5D3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W5D3Application.class, args);
+	}
+
+}
